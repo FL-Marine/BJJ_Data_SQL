76b2b3a9b4879abc27ee0deb2f8af448df06f86e
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2021-12-08 17:14:29 -0500

    Update schema-sql.sql

--- tournament 11_06_21/schema-sql.sql
+++ tournament 11_06_21/schema-sql.sql
@@ -1,2 +1,12 @@
 CREATE SCHEMA bjj_tournament;
 SET search_path = bjj_tournament;
+
+CREATE TABLE identifiers (
+  "match_id" INTEGER,
+  "move_id" INTEGER
+ );
+ 
+ INSERT INTO identifiers
+ 	("match_id", "move_id")
+ VALUES
+ 
