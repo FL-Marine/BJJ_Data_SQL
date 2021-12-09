a4ba3034b02ebb92fb5b437dbd39eebdc949cc04
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2021-12-09 07:03:42 -0500

    Update schema-sql.sql

--- tournament 11_06_21/schema-sql.sql
+++ tournament 11_06_21/schema-sql.sql
@@ -3,10 +3,14 @@ SET search_path = bjj_tournament;
 
 CREATE TABLE identifiers (
   "match_id" INTEGER,
-  "move_id" INTEGER
+  "move_id" INTEGER,
+  "tournament_date" DATE,
+  "opponent_name" VARCHAR(50),
+  "weight_class" VARCHAR(50),
+  "match_style" VARCHAR(50)
  );
  
  INSERT INTO identifiers
- 	("match_id", "move_id")
+ 	("match_id", "move_id",  "tournament_date", "opponent_name", "weight_class",  "match_style")
  VALUES
- 
+ ('1', '1', '2021-11-06', 'daniel_canizales', 'cruiserweight', 'gi'), 
