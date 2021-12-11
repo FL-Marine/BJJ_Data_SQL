d4ddf376b538e78e7ff0481c510201e92c46626f
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2021-12-11 13:42:59 -0500

    Update schema-sql.sql

--- tournament 11_06_21/schema-sql.sql
+++ tournament 11_06_21/schema-sql.sql
@@ -1,15 +1,56 @@
 CREATE SCHEMA bjj_tournament;
 SET search_path = bjj_tournament;
 
-CREATE TABLE identifiers (
-  "match_id" VARCHAR(1),
-  "move_id" VARCHAR(1),
-  "tournament_date" DATE,
-  "weight_class" VARCHAR(50),
-  "match_style" VARCHAR(2)
+ CREATE TABLE tournament_facts (
+   "tournament_date" DATE,
+   "weight_class" VARCHAR(50),
+   "weight_lbs" INTEGER
  );
  
- INSERT INTO identifiers
- 	("match_id", "move_id",  "tournament_date", "weight_class",  "match_style")
- VALUES
- ('1', 'A', '2021-11-06', 'cruiserweight', 'gi') 
+INSERT INTO tournament_facts
+   ("tournament_date",  "weight_class",
+    "weight_lbs")
+VALUES
+ ('2021-11-06', 'cruiserweight', '182');
+ 
+CREATE TABLE match_style (
+   "match_style" VARCHAR(10)
+ );
+ 
+INSERT INTO  match_style
+ 	("match_style")
+VALUES
+   	('gi'),
+    ('nogi');
+    
+CREATE TABLE moves (
+    "move_id" INTEGER,
+    "move_name" VARCHAR(50)
+  );
+    
+INSERT INTO moves 
+    ("move_id", "move_name")
+VALUES
+     ('1', 'guard_pull'),
+     ('2', 'single_leg'),
+     ('3', 'double_leg'),
+     ('4', 'arm_drag'),
+  	 ('5', 'trip'),
+     ('6', 'lateral_throw'),
+     ('7', 'guard'),
+     ('8',	'half_guard'),
+     ('9', 'side_control'),
+     ('10', 'mount'),
+     ('11', 'back'),
+     ('12',	'sweep'),
+     ('13',	'guard_pass'),
+     ('14',	'arm_bar'),
+     ('15',	'americana'),
+     ('16',	'kimura'),
+     ('17',	'rear_naked'),
+     ('18',	'front_collar'),
+     ('19',	'triangle'),
+     ('20',	'arm_triangle'),
+     ('21',	'guillotine'),
+     ('22',	'other');
+    
