4a71a2406c2cf633f106d93a56eaf487ad314d94
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2021-12-14 05:38:02 -0500

    Update schema-sql.sql

--- tournament 11_06_21/schema-sql.sql
+++ tournament 11_06_21/schema-sql.sql
@@ -20,39 +20,57 @@ CREATE TABLE match_style (
 INSERT INTO  match_style
  	("match_style")
 VALUES
-  	('gi'), ('nogi');
+   	('gi'),
+    ('nogi');
     
 CREATE TABLE moves (
     "move_id" INTEGER,
-    "move_name" VARCHAR(50)
+    "move_name" VARCHAR(50),
+  	"category_id" INTEGER,
+  	"category_name" VARCHAR(50)
   );
     
 INSERT INTO moves 
-    ("move_id", "move_name")
+    ("move_id", "move_name",  "category_id", "category_name")
 VALUES
-     ('1', 'guard_pull'),
-     ('2', 'single_leg'),
-     ('3', 'double_leg'),
-     ('4', 'arm_drag'),
-     ('5', 'trip'),
-     ('6', 'lateral_throw'),
-     ('7', 'guard'),
-     ('8', 'half_guard'),
-     ('9', 'side_control'),
-     ('10', 'mount'),
-     ('11', 'back'),
-     ('12', 'sweep'),
-     ('13', 'guard_pass'),
-     ('14', 'arm_bar'),
-     ('15', 'americana'),
-     ('16', 'kimura'),
-     ('17', 'rear_naked'),
-     ('18', 'front_collar'),
-     ('19', 'triangle'),
-     ('20', 'arm_triangle'),
-     ('21', 'guillotine'),
-     ('22', 'other');
-    
-   	
-   
+     ('1', 'guard pull', '1', 'takedown'),
+     ('2', 'single leg', '2', 'position'),
+     ('3', 'double leg', '3', 'transition'),
+     ('4', 'arm drag', '4', 'submission arm'),
+  	 ('5', 'trip', '5', 'choke'),
+     ('6', 'lateral throw', null, ''),
+     ('7', 'guard', null, ''),
+     ('8', 'half guard', null, ''),
+     ('9', 'side control', null, ''),
+     ('10', 'mount', null, ''),
+     ('11', 'back', null, ''),
+     ('12', 'sweep', null, ''),
+     ('13', 'guard pass', null, ''),
+     ('14', 'arm bar', null, ''),
+     ('15', 'americana', null, ''),
+     ('16', 'kimura', null, ''),
+     ('17', 'rear naked', null, ''),
+     ('18', 'front collar', null, ''),
+     ('19', 'triangle', null, ''),
+     ('20', 'arm_triangle', null, ''),
+     ('21', 'guillotine', null, ''),
+     ('22', 'other', null, '');
+     
+
+
+CREATE TABLE match_results (
+  "points_scored" INTEGER,
+  "points_given_up" INTEGER,
+  "results" VARCHAR(2),
+  "match_id" INTEGER
+ );
+ 
+ INSERT INTO match_results
+ ("points_scored", "points_given_up", "results", "match_id")
+ VALUES
+ 	('36', '11', 'W', '1'),
+    (null, null, 'W', '2'),
+    (null, null, 'W', '3'),
+   	(null, null, 'W', '4'),
+   (null, null, 'L', '5');
  
