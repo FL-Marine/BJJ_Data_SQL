3afb68026c7a2b16c48d3f615f106d8b22882c9c
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-02-03 16:18:28 -0500

    Update query-sql.sql

--- tournament 11_06_21/query-sql.sql
+++ tournament 11_06_21/query-sql.sql
@@ -2,8 +2,8 @@ Offensive & Defensive points per match
 SELECT points_scored / 5 AS points_scored_per_match, points_given_up / 5 AS points_lost_per_match
 FROM bjj_tournament.match_results
 WHERE points_scored IS NOT NULL;
-/* Scored an avg of 7 points per match
-Gave up an avg of just 2 points per match */
+-- Scored an avg of 7 points per match
+-- Gave up an avg of just 2 points per match 
 
 | points_scored_per_match | points_lost_per_match |
 | ----------------------- | --------------------- |
