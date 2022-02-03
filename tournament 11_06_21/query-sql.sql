cab15f85c17d43a673289ac5421704cdc8312c9e
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-02-03 16:16:14 -0500

    Update query-sql.sql

--- tournament 11_06_21/query-sql.sql
+++ tournament 11_06_21/query-sql.sql
@@ -1,9 +1,7 @@
-
-
-
-    SELECT points_scored / 5 AS points_scored_per_match, points_given_up / 5 AS points_lost_per_match
-    FROM bjj_tournament.match_results
-    WHERE points_scored IS NOT NULL;
+** Points **
+SELECT points_scored / 5 AS points_scored_per_match, points_given_up / 5 AS points_lost_per_match
+FROM bjj_tournament.match_results
+WHERE points_scored IS NOT NULL;
 
 | points_scored_per_match | points_lost_per_match |
 | ----------------------- | --------------------- |
