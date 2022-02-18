8a6feea98adedbf5161bd930e54d0d14e326cca5
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-02-18 06:37:32 -0500

    Update ERD-sql.sql

--- tournament 11_06_21/ERD-sql.sql
+++ tournament 11_06_21/ERD-sql.sql
@@ -36,6 +36,3 @@ CREATE TABLE "match_points" (
 ALTER TABLE "match_results" ADD FOREIGN KEY ("match_id") REFERENCES "match_points" ("match_id");
 
 ALTER TABLE "match_points" ADD FOREIGN KEY ("move_id") REFERENCES "moves" ("move_id");
-
-Link to ERD - https://dbdiagram.io/d/620f7f35485e433543d7f19b
-
