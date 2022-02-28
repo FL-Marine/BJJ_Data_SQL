19760ad15fcb640446ce47c6615e626a26bf7b89
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-02-28 16:07:06 -0500

    Update query-sql.sql

--- tournament 11_06_21/query-sql.sql
+++ tournament 11_06_21/query-sql.sql
@@ -1,3 +1,20 @@
+**Tournament Facts**
+SELECT *
+FROM bjj_tournament.tournament_facts;
+
+| tournament_date          | weight_class  | weight_lbs |
+| ------------------------ | ------------- | ---------- |
+| 2021-11-06T00:00:00.000Z | cruiserweight | 182        |
+
+SELECT *
+FROM bjj_tournament.match_style;
+
+| match_style |
+| ----------- |
+| gi          |
+| nogi        |
+
+
 **Offensive & Defensive points per match**
 SELECT points_scored / 5 AS points_scored_per_match, points_given_up / 5 AS points_lost_per_match
 FROM bjj_tournament.match_results
