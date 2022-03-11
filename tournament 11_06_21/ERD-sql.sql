cf5243e38710d196d79fb946854361698f4d9f03
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2022-03-11 08:01:40 -0500

    Update ERD-sql.sql

--- tournament 11_06_21/ERD-sql.sql
+++ tournament 11_06_21/ERD-sql.sql
@@ -1,38 +1,48 @@
-CREATE TABLE "tournament_facts" (
-  "tournament_date" DATE,
-  "weight_class" VARCHAR(50),
+Table "tournament_facts" {
+  "tournament_date" DATE
+  "weight_class" VARCHAR(50)
   "weight_lbs" INTEGER
-);
+}
 
-CREATE TABLE "match_style" (
+Table "match_style" {
   "match_style" VARCHAR(10)
-);
+}
 
-CREATE TABLE "moves" (
-  "move_id" VARCHAR(2),
-  "move_name" VARCHAR(50),
-  "category_id" VARCHAR(3),
-  "category_name" VARCHAR(50)
-);
-
-CREATE TABLE "match_results" (
-  "points_scored" INTEGER,
-  "points_given_up" INTEGER,
-  "results" VARCHAR(2),
-  "match_id" VARCHAR(2),
+Table "moves" {
   "move_id" VARCHAR(2)
-);
+  "move_name" VARCHAR(50)
+  "category_id" VARCHAR(3)
+  "category_name" VARCHAR(50)
+}
 
-CREATE TABLE "match_points" (
-  "move_name" VARCHAR(50),
-  "move_id" VARCHAR(2),
-  "offense_attempted" INTEGER,
-  "offense_succeeded" INTEGER,
-  "defense_attempted" INTEGER,
-  "defense_succeeded" INTEGER,
+Table "match_results" {
+  "points_scored" INTEGER
+  "points_given_up" INTEGER
+  "results" VARCHAR(2)
   "match_id" VARCHAR(2)
-);
+  "move_id" VARCHAR(2)
+}
 
-ALTER TABLE "match_results" ADD FOREIGN KEY ("match_id") REFERENCES "match_points" ("match_id");
+Table "match_points" {
+  "move_name" VARCHAR(50)
+  "move_id" VARCHAR(2)
+  "offense_attempted" INTEGER
+  "offense_succeeded" INTEGER
+  "defense_attempted" INTEGER
+  "defense_succeeded" INTEGER
+  "match_id" VARCHAR(2)
+}
 
-ALTER TABLE "match_points" ADD FOREIGN KEY ("move_id") REFERENCES "moves" ("move_id");
+TABLE "points_scoring_system" {
+  "move_score" INTEGER
+  "move_name" VARCHAR(50)
+  "category_name" VARCHAR(50)
+   }
+
+Ref: "match_points"."match_id" < "match_results"."match_id"
+
+Ref: "moves"."move_id" < "match_points"."move_id"
+
+Ref: "points_scoring_system"."move_name" < "match_points"."move_name"
+
+Ref: "points_scoring_system"."category_name" < "moves"."category_name"
